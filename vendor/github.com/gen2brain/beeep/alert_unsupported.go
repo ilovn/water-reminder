c3f75415d6c1cc2511b5082976af3ffeeb0e312f
Yong Zhao <cat@cat.gd>
2026-05-05 18:17:50 +0800

    feat: 添加配置热加载、.env支持和Git忽略配置

--- vendor/github.com/gen2brain/beeep/alert_unsupported.go
+++
@@ -1,8 +0,0 @@
-//go:build !linux && !freebsd && !netbsd && !openbsd && !windows && !darwin && !illumos && !js
-
-package beeep
-
-// Alert displays a desktop notification and plays a beep.
-func Alert(title, message string, icon any) error {
-	return ErrUnsupported
-}
--- vendor/github.com/gen2brain/beeep/notify_unsupported.go
+++
@@ -1,8 +0,0 @@
-//go:build !linux && !freebsd && !netbsd && !openbsd && !windows && !darwin && !illumos && !js
-
-package beeep
-
-// Notify sends desktop notification.
-func Notify(title, message string, icon any) error {
-	return ErrUnsupported
-}
